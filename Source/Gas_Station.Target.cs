5ab2869081c68a1446a1e1aa4889210be12b3605
YaSolo <207256766+YaSolo434@users.noreply.github.com>
2026-09-11 17:37:42 +0400

    to 5.8.2

--- Source/Gas_Station.Target.cs
+++ Source/Gas_Station.Target.cs
@@ -8,8 +8,8 @@ public class Gas_StationTarget : TargetRules
 	public Gas_StationTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("Gas_Station");
 	}
 }
